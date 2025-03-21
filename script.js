b4dc981508ce91c951b2a965588eea483be9a1b2
Fernando Alexandre da Silva <fernando17034@gmail.com>
2025-03-20 22:22:50 -0300

    1.0

---
+++ script.js
@@ -0,0 +1,19 @@
+//comenta
+/* bloco de comentario */
+var primeiroTipo;
+let segundoTipo;
+const terceiroTipo = 10;
+
+var nome="fernando";
+var idade = 29;
+
+console.log(nome);
+console.log(idade);
+
+nome=idade;
+console.log(nome);
+
+function escrever(){
+    document.getElementById('textoGrande').innerText = "oi, vc me clicou";
+    document.getElementById('imagem').src=""; //pra por imagem 
+}
